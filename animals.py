b5a9f67040d8b2e12c6f7c1bdf5a528fa2d0bc76
vjezk <vikjezek@gmail.com>
2024-08-21 19:48:39 +0200

    vylepseno animals.py

--- animals.py
+++ animals.py
@@ -1,12 +1,24 @@
 class Zvire:
-    def __init__(self, jmeno: str, strava: list, output, death_reward):
+    def __init__(
+            self, 
+            jmeno: str, 
+            strava: list, 
+            output: list, 
+            death_reward: list):
         self.jmeno = jmeno
         self.strava = strava
         self.output = output
         self.death_reward = death_reward
+
+        self.hunger = 0
+        self.thirst = 0
         
         self.is_alive = True
 
 class Krava(Zvire):
     def __init__(self, jmeno: str):
-        super().__init__(jmeno, ["tráva"], [["mléko", 5]], [["kůže", 20], ["maso", 20]])
+        super().__init__
+        (jmeno, 
+         ["tráva"], 
+         [["mléko", 5]], 
+         [["kůže", 20], ["maso", 20]])
